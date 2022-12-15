f3b476ad08f0b87bd5abf706bb55b8afe006b8cf
SaiVenkat12 <gsaiv1001@gmail.com>
2022-12-15 23:40:23 +0530

    [SaiVenkat]- EnQueue elements into Queue

---
+++ StackandQueue/LinkedListQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackandQueue
+{
+    internal class LinkedListQueue
+    {
+        Node head = null;
+        public void EnQueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine(node.data + " is inserted into the Queue");
+            }
+        }
+        public void display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("Queue ");
+                Node temp = head;
+                while (temp != null && temp.data != null)
+                {
+                    Console.WriteLine(temp.data+" ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- StackandQueue/Program.cs
+++ StackandQueue/Program.cs
@@ -5,19 +5,27 @@ namespace StackandQueue
     {
         static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);
-            linkedListStack.push(30);
-            linkedListStack.push(56);
-            linkedListStack.Display();
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.EnQueue(56);
+            queue.EnQueue(30);
+            queue.EnQueue(70);
+            queue.display();
 
-            linkedListStack.peak();
 
-            linkedListStack.pop();
-            linkedListStack.Display();
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);
+            //linkedListStack.Display();
 
-            linkedListStack.isEmpty();
-            linkedListStack.Display();
+            //linkedListStack.peak();
+
+            //linkedListStack.pop();
+            //linkedListStack.Display();
+
+            //linkedListStack.isEmpty();
+            //linkedListStack.Display();
+
         }
     }
 }
